64ad9a4cdfa0e4e1d7557fc89d86afbce8052f0d
mubee <mm>
2021-11-17 13:12:55 +0530

    sam

--- DEMO/src/Hello.java
+++ DEMO/src/Hello.java
@@ -7,9 +7,7 @@ public class Hello {
 		System.out.println("helloworld");
 		System.out.println("hi aaam ");
 		System.out.println("sam");
-		System.out.println("sam");
-		System.out.println("i am from space");
-		System.out.println("thankyou ");
+		
 
 	}
 
